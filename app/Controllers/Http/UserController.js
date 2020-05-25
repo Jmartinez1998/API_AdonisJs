a8f72481f06f5c97104988a61fe3a8f4c33e0afb
Jose Luis Martinez <jmartinez@zarotransportation.com>
2020-05-25 14:20:18 -0500

    configuracion mysql y controller createuser

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,11 +1,21 @@
 'use strict'
 
+//Importar modelo User
+const User =use('App/Models/User');
+
 class UserController {
-    store() {
-        return {
+    async store({ request }) {
+        /*return {
             mensaje:"Cremos el user desde el controller!"
-        }
-    }
+        }*/
+        const { email, password} = request.all();
+        const user= await User.create({
+            email,
+            password,
+            username: email
+        });
+        return user;
+    };
 }
 
 module.exports = UserController
