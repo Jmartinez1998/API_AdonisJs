84397cabb485ecc342a0f78b2c1c8d791112bf50
Luis-Montes <mecsi_montes@hotmail.com>
2020-05-26 19:05:46 -0500

    :D

--- app/Controllers/Http/ComentarioController.js
+++ app/Controllers/Http/ComentarioController.js
@@ -2,9 +2,10 @@
 const Comentario = use('App/Models/Comment');
 class ComentarioController {
     async store({ response, request }) {
-        const { fk_user, message, affair, status} = request.all();
+        const { comments, fk_users, message, affair, status} = request.all();
         const comments = await Comentario.create({
-            fk_user,
+            comments,
+            fk_users,
             message,
             affair,
             status
---
+++ database/migrations/1590536643952_comentarios_schema.js
@@ -0,0 +1,24 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class ComentariosSchema extends Schema {
+  up () {
+    this.create('comentarios', (table) => {
+      table.increments()
+      table.string('comments', 200)
+      table.integer('fk_users').unsigned().references('id').inTable('users')
+      table.string('message')
+      table.string('affair')
+      table.integer('status')
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('comentarios')
+  }
+}
+
+module.exports = ComentariosSchema
