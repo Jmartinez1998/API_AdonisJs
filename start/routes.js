c653d36d3307dd08066b2783952ad75607954542
Jesus Garcia Marin <40584584+Crash2906@users.noreply.github.com>
2020-06-01 15:12:35 -0500

    no importa
    acomodo alt +shift f XD

--- start/routes.js
+++ start/routes.js
@@ -26,7 +26,7 @@ Route.get('/pepe', () => {
 })
 Route.post('users/login', 'UserController.login');
 //Agrupamos las rutas
-Route.group(()=> {
+Route.group(() => {
   Route.post('users/login', 'UserController.login');
   // Ruta del registroo nvo usuario
   Route.post('users/regis', 'UserController.store');
@@ -51,10 +51,10 @@ Route.group(()=> {
   Route.get('comentarios', async () => {
     return await Database.table('comments').select('*')
   })
-   //CRUD Escuelas
+  //CRUD Escuelas
   Route.post('schools/new', 'SchoolController.create');
   Route.post('schools/update/:id', 'SchoolController.update');
-   Route.delete('schools/delete/:id', 'SchoolController.destroy');
+  Route.delete('schools/delete/:id', 'SchoolController.destroy');
   //Muestra los comentarios
   Route.get('comentarios', async () => {
     return await Database.table('comentarios').select('*')
@@ -62,10 +62,10 @@ Route.group(()=> {
 }).prefix('api/').middleware(['auth']);;//eto chi
 
 //jesus Rutas we
-Route.group(()=>{
+Route.group(() => {
   Route.post('New', 'PackController.Store')
   Route.get('Take', 'PackController.getAllPack')
   Route.put('Update', 'PackController.Update')
   Route.delete('Delete', 'PackController.Delete')
-  
+
 }).prefix('Packs/').middleware(['auth']);
