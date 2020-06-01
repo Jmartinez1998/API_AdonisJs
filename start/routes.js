c63c0183ad0e098d1a6a777183b0ac4cf284c71a
karla burciaga delgado <karla_burciaga_d@outlook.com>
2020-06-01 13:18:17 -0500

    update schools

--- start/routes.js
+++ start/routes.js
@@ -25,22 +25,22 @@ Route.get('/pepe', () => {
   return { greeting: 'Hello pepe in JSON' }
 })
 Route.post('users/login', 'UserController.login');
-//Agrupamos las rutas 
+//Agrupamos las rutas
 //Agrupamos las rutas
 Route.group(()=> {
   // Ruta del registroo nvo usuario
   Route.post('users/regis', 'UserController.store');
-  Route.delete('users/delete/:id', 'UserController.destroy');
+
   //Ruta del Login
   //Route.post('users/login', 'UserController.login');
   //Crea nva categoria
   Route.post('newCategory', 'CategoryProductController.store');
   Route.get('verus', 'CategoryProductController.index');
-  
+
   Route.get('/categorias', async () => {
     return await Database.table('category_products').select('*')
   })
-  //Agrega nmnvo producto
+  //Agrega nmnvo productoRoute.delete('users/delete/:id', 'UserController.destroy');
   Route.post('Newproducts', 'ProductController.store');
   //Comentarios agregar
   Route.post('newComment', 'CommentController.com')
@@ -53,9 +53,10 @@ Route.group(()=> {
   })
    //CRUD Escuelas
   Route.post('schools/new', 'SchoolController.create');
-  Route.get('schools/update/:id', 'SchoolController.update');
+  Route.post('schools/update/:id', 'SchoolController.update');
+  Route.delete('schools/delete/:id', 'SchoolController.destroy');
   //Muestra los comentarios
   Route.get('comentarios', async () => {
     return await Database.table('comentarios').select('*')
   })
-}).prefix('api/').middleware(['auth']);
+}).prefix('api/');
